b91d7980a0a924d814b09fd2be398821186b5109
tisha-akter <tishaakter.jui79@gmail.com>
2023-05-03 23:44:11 +0600

    added style recipe details

--- src/pages/CardDetails/Recipes/RecipeDetails.jsx
+++ src/pages/CardDetails/Recipes/RecipeDetails.jsx
@@ -4,23 +4,25 @@ import { FaHeart } from 'react-icons/fa';
 const RecipeDetails = ({ chefDetails }) => {
 
     return (
-        <div className='mb-5 '>
-            <section className="text-gray-600 body-font overflow-hidden">
+        <div>
+
+            <section className="text-gray-600 body-font overflow-hidden ">
                 <div className="container px-5 py-24 mx-auto gap-5">
+                    <h2 className='text-center text-orange-500 font-sans text-5xl mb-12'> This Chef's Famous recipes</h2>
                     {chefDetails?.recipes?.map((item, index) => (
-                        <div key={index} className="lg:w-4/5 mx-auto flex flex-wrap">
+                        <div key={index} className="lg:w-4/5 mx-auto flex flex-wrap mb-10 p-5 border border-orange-500">
                             <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
 
-                                <h1 className="text-gray-900 text-3xl title-font font-medium text-orange-500 mb-4">{item?.recipe}</h1>
+                                <h1 className="text-3xl title-font font-medium text-orange-500 mb-4">{item?.recipe}</h1>
                                 <div className="flex mb-4">
                                     <a className="flex-grow  border-b-2 border-orange-500 py-2 font-bold  px-1"><span className='text-orange-500 text-xl'>Ingredients: </span><br />{item?.ingredients}</a>
 
                                 </div>
-                                
+
                                 <p className="leading-relaxed mb-4 font-bold"><span className='text-orange-500 text-xl'>Cooking method:</span><br /> {item?.cooking_method}</p>
 
                                 <div className="flex">
-                                    <span className="title-font font-medium text-2xl text-gray-900">rating{}</span>
+                                    <span className="title-font font-medium text-2xl text-gray-900">rating{ }</span>
                                     <button className="flex ml-auto text-white bg-orange-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"><FaHeart flex ml-auto></FaHeart></button>
 
                                 </div>
